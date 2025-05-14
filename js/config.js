7eef2e3230509bc2542ea8128e2e11842e063545
eiregolfclub <eiregolfclub@gmail.com>
2025-05-14 22:53:21 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  API_URL: "https://api.eiregolfclub.ie", // ← change to your backend
+  MAPBOX_TOKEN: "pk.your_mapbox_public_key",
+  SOCIAL_REFRESH_MS: 5 * 60 * 1000, // 5 min
+  STORAGE_TOKEN_KEY: "egc_token",
+};
